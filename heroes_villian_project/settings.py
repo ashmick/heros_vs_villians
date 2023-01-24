2c8b2b9735482f2bec3c04e773d4ab7c7d1d126e
ashmick <ashley.mcneill2014@gmail.com>
2023-01-24 12:21:17 -0500

    created super app, super_type app, added post collection to files

--- heroes_villian_project/settings.py
+++ heroes_villian_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5zfu@9d4eg9si99449&+qbndw*+c*)j0y9f3#)nb%(+(pbt2!s'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -73,12 +70,7 @@ WSGI_APPLICATION = 'heroes_villian_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
