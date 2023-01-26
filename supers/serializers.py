41f044f1d46482b29c2d013aaac7625c74f30364
ashmick <ashley.mcneill2014@gmail.com>
2023-01-25 21:15:49 -0500

    updated serializer to make delete for postman work correctly added type id to field

--- supers/serializers.py
+++ supers/serializers.py
@@ -5,7 +5,7 @@ class SuperSerializer (serializers.ModelSerializer):
         class Meta:
             model= Supers
             fields = '__all__'
-            field=['name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
+            field=['name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type', 'super_type_id']
             depth=1
-            
-super_type_id=serializers.IntegerField(write_only=True)
+
+        super_type_id=serializers.IntegerField(write_only=True)
